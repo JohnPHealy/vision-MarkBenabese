869cac7b29e291bae9b0e7105a06fd9e8b1b9c21
Mark Benabese <C20710145@mytudublin.ie>
2021-05-07 14:56:32 +0100

    Timer Added and Enemy AI with collision

---
+++ Vision Game/Assets/Scripts/EndScreen.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndScreen : MonoBehaviour
+{
+    public GameObject endScreen;
+    // Start is called before the first frame update
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            endScreen.gameObject.SetActive(true);
+        }
+    }
+}
